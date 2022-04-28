613d2c30e23c728c4a921f1642835d4f2e51e83b
ram prakash <60039795+ramprakash7342@users.noreply.github.com>
2022-04-28 17:14:11 +0530

    Delete Case_Study directory

--- Case_Study/CartService/src/main/java/com/casestudy/cartservice/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.casestudy.cartservice.entity;
-
-public enum Role {
-
-	ROLE_CUSTOMER, ROLE_MERCHANT, ROLE_DELIVERYAGENT;
-}
--- Case_Study/ProfileService2/src/main/java/com/casestudy/profileservice/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.casestudy.profileservice.entity;
-
-public enum Role {
-
-	ROLE_CUSTOMER, ROLE_MERCHANT, ROLE_DELIVERYAGENT;
-}
